5adaf864403f6c572e8de560a2d190e5aec27a28
Van Nguyen <vanh.nguyen@gsa.gov>
2022-03-21 15:05:09 -0400

    more output improvements

--- ci/scan-container.sh
+++ ci/scan-container.sh
@@ -7,10 +7,10 @@ grype ${IMAGE} -q -o table --file table.txt
 cat output.json | jq '.matches | .[]? |  .vulnerability.severity' >> severity.txt
 
 
-echo "Critical:" | grep -o -i critical severity.txt | wc -l
-echo "High:" | grep -o -i high severity.txt | wc -l
-echo "Medium:" | grep -o -i medium severity.txt | wc -l
-echo "Low:" | grep -o -i low severity.txt | wc -l
-echo "Negligible:" | grep -o -i negligible severity.txt | wc -l
+echo -n "Critical: " | grep -o -i critical severity.txt | wc -l
+echo -n "High: " | grep -o -i high severity.txt | wc -l
+echo -n "Medium: " | grep -o -i medium severity.txt | wc -l
+echo -n "Low: " | grep -o -i low severity.txt | wc -l
+echo -n "Negligible: " | grep -o -i negligible severity.txt | wc -l
 
 cat table.txt
