11b54594d2d62616908ee30fce9ab84b80f1ba4a
David Anderson <david.n.anderson@gsa.gov>
2022-04-26 18:19:38 -0400

    make command one line

--- ci/list-images.sh
+++ ci/list-images.sh
@@ -2,8 +2,6 @@
 
 set -e -x
 
-aws ecr list-images --repository-name $IMAGE_REPOSITORY > list.txt
-
-cat list.txt | jq -r .imageIds[].imageTag > image-list/tags.txt
+aws ecr list-images --repository-name $IMAGE_REPOSITORY | jq -r .imageIds[].imageTag > image-list/tags.txt
 
 cat image-list/tags.txt
