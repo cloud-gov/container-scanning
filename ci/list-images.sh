ec205f01078ec8ffd9a67647b7285ec30ae497a0
David Anderson <david.n.anderson@gsa.gov>
2022-04-27 10:12:17 -0400

    add file to s3

--- ci/list-images.sh
+++ ci/list-images.sh
@@ -4,4 +4,6 @@ set -e -x
 
 aws ecr list-images --repository-name $IMAGE_REPOSITORY | jq -r .imageIds[].imageTag > image-list/tags.txt
 
+aws s3 cp image-list/tags.txt "s3://${S3_TFSTATE_BUCKET}/${STACK_NAME}/tags.txt"
+
 cat image-list/tags.txt
