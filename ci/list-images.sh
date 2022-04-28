1644273b64c4878eb845b5835ff17d5431a843b6
David Anderson <david.n.anderson@gsa.gov>
2022-04-28 11:13:46 -0400

    update pipeline

--- ci/list-images.sh
+++ ci/list-images.sh
@@ -2,8 +2,8 @@
 
 set -e -x
 
-aws ecr list-images --repository-name $IMAGE_REPOSITORY | jq -r '[.imageIds[].imageTag]' > image-list/tags.json
+aws ecr list-images --repository-name $IMAGE_REPOSITORY | jq -r '[.imageIds[].imageTag]' > image-list/tags-$IMAGE_REPOSITORY.json
 
-aws s3 cp image-list/tags.json "s3://${S3_TFSTATE_BUCKET}/${STACK_NAME}/tags.json"
+aws s3 cp image-list/tags-$IMAGE_REPOSITORY.json "s3://${S3_TFSTATE_BUCKET}/${STACK_NAME}/tags-${IMAGE_REPOSITORY}.json"
 
-cat image-list/tags.json
+cat image-list/tags-$IMAGE_REPOSITORY.json
