49f6da18fe9dffe8187598f6df64563b1498a37a
David Anderson <david.n.anderson@gsa.gov>
2022-04-28 10:54:44 -0400

    add docker and login

--- ci/scan-ecr-container.sh
+++ ci/scan-ecr-container.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#install docker
+apt-get update
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#get ecr image
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY}
+
+#scan
 grype ${IMAGE} -q -o json --file output.json
 grype ${IMAGE} -q -o table --file table.txt
 
