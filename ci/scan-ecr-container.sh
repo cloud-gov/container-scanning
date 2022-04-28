42cdee3a2ce6590e384f19e39e1dc20c9f3fe51e
David Anderson <david.n.anderson@gsa.gov>
2022-04-28 10:59:15 -0400

    add y to script

--- ci/scan-ecr-container.sh
+++ ci/scan-ecr-container.sh
@@ -6,7 +6,7 @@ apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
@@ -15,7 +15,7 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 #get ecr image
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY}
