c121fbce463e55ab57e96ed45d114f7aed69d681
David Anderson <david.n.anderson@gsa.gov>
2022-05-20 14:07:03 -0400

    add pass/fail testing

--- ci/scan-container.sh
+++ ci/scan-container.sh
@@ -5,11 +5,21 @@ grype ${IMAGE} -q -o table --file table.txt
 
 cat cves/output.json | jq '.matches | .[]? |  .vulnerability.severity' >> severity.txt
 
+critical=$(grep -o -i critical severity.txt | wc -l)
+high=$(grep -o -i high severity.txt | wc -l)
+medium=$(grep -o -i medium severity.txt | wc -l)
+low=$(grep -o -i low severity.txt | wc -l)
+negligible=$(grep -o -i negligible severity.txt | wc -l)
 
-echo -n "Critical: " && grep -o -i critical severity.txt | wc -l
-echo -n "High: " && grep -o -i high severity.txt | wc -l
-echo -n "Medium: " && grep -o -i medium severity.txt | wc -l
-echo -n "Low: " && grep -o -i low severity.txt | wc -l
-echo -n "Negligible: " && grep -o -i negligible severity.txt | wc -l
+echo -n "Critical: $critical"
+echo -n "High: $high"
+echo -n "Medium: $medium"
+echo -n "Low: $low"
+echo -n "Negligible: $negligible"
 
 cat table.txt
+
+if [ $critical -ge 1 || $high -ge 1]
+then echo -n "Review needed for image ${IMAGE}. Successfully uploaded ${IMAGE} image to ECR" > message/alert.txt
+else echo -n "Successfully uploaded ${IMAGE} image to ECR" > message/alert.txt
+fi
