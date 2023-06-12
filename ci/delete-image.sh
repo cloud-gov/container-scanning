31c32461ebd8a49a7bb7814f42651d992a9c094c
David Anderson <david.n.anderson@gsa.gov>
2023-06-12 15:45:27 -0400

    add missing bracket

--- ci/delete-image.sh
+++ ci/delete-image.sh
@@ -5,4 +5,4 @@ set -e -x
 aws ecr batch-delete-image \
      --repository-name ${IMAGE_REPOSITORY} \
      --image-ids imageTag=${IMAGE_TAG} \
-     --region ${AWS_DEFAULT_REGION
+     --region ${AWS_DEFAULT_REGION}
